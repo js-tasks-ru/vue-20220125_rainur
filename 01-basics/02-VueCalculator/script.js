227f37a9d1ae07971bbb6b91aaf5f1463ba8b30a
Rainur Khamidullin <rainur@mail.ru>
2022-01-31 07:31:24 +0300

    Решение задач блока 00 - intro и 01-basics

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,40 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const Root = defineComponent({
+  name: 'Root',
+  // Реактивные данные приложения, его локальное состояние
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      operator: null,
+    };
+  },
+
+  computed: {
+    calculate: function () {
+      let result = 0;
+      if (this.operator == 'sum') {
+        result = this.operand1 + this.operand2;
+      } else if (this.operator == 'subtract') {
+        result = this.operand1 - this.operand2;
+      } else if (this.operator == 'multiply') {
+        result = this.operand1 * this.operand2;
+      } else if (this.operator == 'divide') {
+        result = this.operand1 / this.operand2;
+      }
+      return result;
+    },
+  },
+});
+
+// Создаём новое приложение по корневому компоненту
+const app = createApp(Root);
+
+// Монтируем приложение на странице
+// Возвращается публичный экземпляр корневого компонента
+const vm = app.mount('#app');
+
+// Добавляем vm в глобальные переменные для простой отладки в консоли браузера
+window.vm = vm;
